48ee2dd2f24a7861aac34b590ffcdf96ec1d3dc1
Maxim <31155487+KozinM@users.noreply.github.com>
2022-10-15 19:31:58 +0300

    major

--- src/index.js
+++ src/index.js
@@ -1,132 +1,54 @@
-/*library connection*/
-import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-/*importing own functions*/
-import './css/styles.css';
-import { createGallery } from './js/createGallery';
-import { cleanGallery } from './js/createGallery';
-import { getImages } from './js/getImages';
-
-/*variables */
-
-/*creating instanse of simplelightbox instance*/
-let simplelightbox = new SimpleLightbox ('.gallery a', {
-  nav: true,
-  close: true,
-  caption: true,
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});;
-
-/*for search reasons*/
-let currentSearch = {
-  page: 1,
-  phrase: '',
-  totalHits: 0,
-  resultsPerPage: 40,
-  nowDisplayed: 0
+input.onfocus = function () {
+  browsers.style.display = 'block';
+  input.style.borderRadius = "5px 5px 0 0";  
 };
-
-/*document elements*/
-const refs = {
-  gallery: document.querySelector('.gallery'),
-  btnSearch: document.querySelector('#search-form'),
-  btnLoadMore: document.querySelector('.load-more'),
-};
-
-//adding event listiner on search button
-refs.btnSearch.addEventListener('submit', clickOnBtnSearchHandler);
-
-//adding event listiner on loadMore button
-refs.btnLoadMore.addEventListener('click', clickOnBtnLoadMoreHandler);
-
-/*defining function for getting images without gallery creation*/ 
-async function getImgWithOutGalleryCreation(searchString, page, perPage) {
-  const { data } = await getImages(searchString, page, perPage);
-  currentSearch.totalHits = data.totalHits;
-  currentSearch.nowDisplayed = page*perPage;
-  return data.hits;
-}
-
-/*defining clickOnBtnSearchHandler function*/
-
-function clickOnBtnSearchHandler(event) {
-  event.preventDefault();
-  cleanGallery(refs.gallery);
-  refs.btnLoadMore.classList.add('js-is-hidden');
-  currentSearch.page = 1;
-  currentSearch.nowDisplayed = 0;
-
-  currentSearch.phrase = event.currentTarget.searchQuery.value.trim();
-  //console.log('in button search handler  ' + currentSearch.phrase);
-
-  if (currentSearch.phrase === '') {
-    return warningEmptySearch();
+for (let option of browsers.options) {
+  option.onclick = function () {
+    input.value = option.value;
+    browsers.style.display = 'none';
+    input.style.borderRadius = "5px";
   }
+};
 
-  //refs.btnLoadMore.classList.remove('js-is-hidden');
-  getImgWithOutGalleryCreation(
-    currentSearch.phrase,
-    currentSearch.page,
-    currentSearch.resultsPerPage
-  )
-    .then(data => {
-      createGallery(data, refs.gallery);
-      if(currentSearch.totalHits>currentSearch.nowDisplayed){
-        //console.log('totalHits: '+ currentSearch.totalHits + ' now displayed: '+ currentSearch.nowDisplayed);
-        refs.btnLoadMore.classList.remove('js-is-hidden');
-      }
-      simplelightbox.refresh();
-    })
-    .then(() => {
-      if (currentSearch.totalHits === 0) {
-        refs.btnLoadMore.classList.add('js-is-hidden');
-        return warningNoResults();
-      }
-    })
-    .catch(error => console.log(error));
+input.oninput = function() {
+  currentFocus = -1;
+  var text = input.value.toUpperCase();
+  for (let option of browsers.options) {
+    if(option.value.toUpperCase().indexOf(text) > -1){
+      option.style.display = "block";
+  }else{
+    option.style.display = "none";
+    }
+  };
 }
-
-/*defining clickOnBtnLoadMoreHandler function*/
-function clickOnBtnLoadMoreHandler(event) {
-  if (currentSearch.totalHits > currentSearch.nowDisplayed) {
-    currentSearch.page += 1;
-    //console.log('in button load more handler  ' + currentSearch.phrase);
-    getImgWithOutGalleryCreation(
-      currentSearch.phrase,
-      currentSearch.page,
-      currentSearch.resultsPerPage
-    ).then(data => {
-      createGallery(data, refs.gallery);
-      if(currentSearch.totalHits<currentSearch.nowDisplayed){
-        //console.log('totalHits: '+ currentSearch.totalHits + ' now displayed: '+ currentSearch.nowDisplayed);
-        refs.btnLoadMore.classList.add('js-is-hidden');
-      }
-      simplelightbox.refresh();
-    })
-    .catch(error => console.log(error));
-  } else {
-    refs.btnLoadMore.classList.add('js-is-hidden');
-    return warningNoMoreResults();
+var currentFocus = -1;
+input.onkeydown = function(e) {
+  if(e.keyCode == 40){
+    currentFocus++
+   addActive(browsers.options);
+  }
+  else if(e.keyCode == 38){
+    currentFocus--
+   addActive(browsers.options);
+  }
+  else if(e.keyCode == 13){
+    e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (browsers.options) browsers.options[currentFocus].click();
+        }
   }
 }
 
-/*notification functions*/
-function warningEmptySearch() {
-  Notiflix.Notify.failure('Please enter a word or phrase for searchig!');
-}
-
-function warningNoResults() {
-  Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
-}
-
-function warningNoMoreResults() {
-  Notiflix.Notify.failure(
-    "We're sorry, but you've reached the end of search results."
-  );
+function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("active");
+  }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("active");
+    }
   }
